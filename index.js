f663a30d11d123fae902e7c58df6bc13016025d4
Mark Vayngrib <mark@tradle.io>
2019-04-25 20:07:08 -0400

    improve stats semantics

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
 
-import { testWithSmallValues, testWithMediumValues, testWithLargeValues } from './test-multi-get'
+import { testWithValues, compareGetVsMultiGet } from './test-multi-get'
 
 setTimeout(async () => {
-	const small = await testWithSmallValues(100)
-	const medium = await testWithMediumValues(100)
-	const large = await testWithLargeValues(100)
+  const twentyBytes = await testWithValues({ n: 100, valueLength: 20 })
+  const twoHundredBytes = await testWithValues({ n: 100, valueLength: 200 })
+  const tenKB = await testWithValues({ n: 100, valueLength: 10000 })
+  const mixed = await compareGetVsMultiGet(
+    Array.from({ length: 100 }).map((_, i) => [String(i), 'a'.repeat(Math.random() * 10000)])
+  )
 
-	Alert.alert('getItem vs multiGet', JSON.stringify({ small, medium, large }, null, 2))
+  Alert.alert(
+    'getItem vs multiGet',
+    JSON.stringify({ twentyBytes, twoHundredBytes, tenKB, mixed }, null, 2)
+  )
 }, 2000)
