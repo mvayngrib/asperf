4e9bff7c014d7e3f7e831d015fd26d685fcc3d27
Mark Vayngrib <mark@tradle.io>
2019-04-25 20:11:23 -0400

    add a case

--- index.js
+++ index.js
@@ -14,12 +14,16 @@ setTimeout(async () => {
   const twentyBytes = await testWithValues({ n: 100, valueLength: 20 })
   const twoHundredBytes = await testWithValues({ n: 100, valueLength: 200 })
   const tenKB = await testWithValues({ n: 100, valueLength: 10000 })
-  const mixed = await compareGetVsMultiGet(
+  const mixedUpTo10KB = await compareGetVsMultiGet(
     Array.from({ length: 100 }).map((_, i) => [String(i), 'a'.repeat(Math.random() * 10000)])
+  )
+
+  const mixedUpTo100KB = await compareGetVsMultiGet(
+    Array.from({ length: 100 }).map((_, i) => [String(i), 'a'.repeat(Math.random() * 100000)])
   )
 
   Alert.alert(
     'getItem vs multiGet',
-    JSON.stringify({ twentyBytes, twoHundredBytes, tenKB, mixed }, null, 2)
+    JSON.stringify({ twentyBytes, twoHundredBytes, tenKB, mixedUpTo10KB, mixedUpTo100KB }, null, 2)
   )
 }, 2000)
